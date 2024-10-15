ce4b08b1682c6ee3606ad182adc2a9377a6e0070
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2024-10-15 20:53:03 +0530

    no scrollbar, prefetching in navbar

--- src/components/component/Navbar.tsx
+++ src/components/component/Navbar.tsx
@@ -29,18 +29,18 @@ const Navbar = () => {
     return (
         <header className={twMerge("absolute z-10 w-full px-4 py-6 sm:px-6 lg:px-8", newClasses)}>
             <div className="container mx-auto flex items-center justify-between">
-                <Link href="/" className="text-lg font-bold text-primary-foreground w-48" prefetch={false}>
+                <Link href="/" className="text-lg font-bold text-primary-foreground w-48">
                     Daishi Kato
                 </Link>
                 <nav className="hidden space-x-4 sm:flex">
                     {navLinks.map((link) => (
-                        <Link href={link.href} key={link.href} className="text-sm font-medium text-primary-foreground hover:underline underline-offset-4" prefetch={false}>
+                        <Link href={link.href} key={link.href} className="text-sm font-medium text-primary-foreground hover:underline underline-offset-4" >
                             {link.name}
                         </Link>
                     ))}
                 </nav>
                 <Button variant={'secondary'} className='w-48 ' ref={consultNavRef}>
-                    <Link href="/#contact" className="text-sm font-medium text-center" prefetch={false}>
+                    <Link href="/#contact" className="text-sm font-medium text-center">
                         Get Consulted
                     </Link>
                 </Button>
